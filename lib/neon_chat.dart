644bc97710ccfd764c0b64cb1a8731157fe2462f
julien <julien@neon.dev>
2022-02-17 20:57:50 +0100

    exports

--- lib/neon_chat.dart
+++ lib/neon_chat.dart
@@ -2,7 +2,10 @@ library neon_chat;
 
 import 'package:neon_chat/src/neon_chat_setup_options.dart';
 
-export 'src/conversation/conversation.dart';
+export 'src/conversation/conversation.dart' show ChatBloc, ChatMessage;
+export 'src/conversations/conversations.dart'
+    show Conversation, ConversationItem;
+export 'src/core/core.dart' show FirebaseUser;
 
 part 'src/neon_chat_impl.dart';
 part 'src/neon_chat_app.dart';
