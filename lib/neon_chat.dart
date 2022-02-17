091a6e23e5d1bec8c5253fdf50a893527553a63a
julien <julien@neon.dev>
2022-02-17 21:31:05 +0100

    refactorings, still no clue

--- lib/neon_chat.dart
+++ lib/neon_chat.dart
@@ -1,9 +1,9 @@
 library neon_chat;
 
-import 'package:neon_chat/src/conversation/presentation/bloc/chat_bloc/chat_bloc.dart';
-import 'package:neon_chat/src/conversation/presentation/bloc/chat_search_bloc/chat_search_bloc.dart';
-import 'package:neon_chat/src/conversations/presentation/bloc/conversations_bloc/conversations_bloc.dart';
-import 'package:neon_chat/src/conversations/presentation/bloc/conversations_search_bloc/conversations_search_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:neon_chat/src/conversation/conversation.dart';
+import 'package:neon_chat/src/conversations/conversations.dart';
+import 'package:neon_chat/src/core/data/repositories/firebase_user_profile_repository_impl.dart';
 import 'package:neon_chat/src/neon_chat_setup_options.dart';
 
 export 'src/conversation/conversation.dart' show ChatMessage;
