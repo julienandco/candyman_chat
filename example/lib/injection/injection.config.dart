607b3b781a556a18a519f3ff219ca9259735a529
julien <julien@neon.dev>
2022-02-24 15:31:46 +0100

    getIt setup for example project

---
+++ example/lib/injection/injection.config.dart
@@ -0,0 +1,36 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:cloud_firestore/cloud_firestore.dart' as _i4;
+import 'package:firebase_auth/firebase_auth.dart' as _i3;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+import 'package:neon_chat/neon_chat.dart' as _i5;
+
+import 'firebase_injectable_module.dart' as _i6;
+import 'remote_data_source_injectable_module.dart'
+    as _i7; // ignore_for_file: unnecessary_lambdas
+
+// ignore_for_file: lines_longer_than_80_chars
+/// initializes the registration of provided dependencies inside of [GetIt]
+_i1.GetIt $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  final remoteDataBaseInjectableModule = _$RemoteDataBaseInjectableModule();
+  gh.lazySingleton<_i3.FirebaseAuth>(
+      () => firebaseInjectableModule.firebaseAuth);
+  gh.lazySingleton<_i4.FirebaseFirestore>(
+      () => firebaseInjectableModule.firebaseFirestore);
+  gh.lazySingleton<_i5.RemoteDataSource>(
+      () => remoteDataBaseInjectableModule.remoteDataSource);
+  return get;
+}
+
+class _$FirebaseInjectableModule extends _i6.FirebaseInjectableModule {}
+
+class _$RemoteDataBaseInjectableModule
+    extends _i7.RemoteDataBaseInjectableModule {}
